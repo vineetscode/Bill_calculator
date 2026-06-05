662279bb16a68096fc0ba23242d7c07fd6b3aae1
vineetscode <vineetsinghjzr28@gmail.com>
2026-06-05 11:21:03 +0530

    listing on google

---
+++ src/app/sittemap.js
@@ -0,0 +1,9 @@
+// src/app/sitemap.js
+export default function sitemap() {
+  return [
+    {
+      url: 'https://flowtix-smart-bill-calculator.netlify.app',
+      lastModified: new Date(),
+    },
+  ];
+}
