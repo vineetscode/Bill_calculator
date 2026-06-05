48f8866e60e7837a4cb8094c9e9f63932575ccb9
vineetscode <vineetsinghjzr28@gmail.com>
2026-06-05 11:25:29 +0530

    Add sitemap

--- src/app/sittemap.js
+++
@@ -1,9 +0,0 @@
-// src/app/sitemap.js
-export default function sitemap() {
-  return [
-    {
-      url: 'https://flowtix-smart-bill-calculator.netlify.app',
-      lastModified: new Date(),
-    },
-  ];
-}
